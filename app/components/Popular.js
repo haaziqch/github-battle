7fb9327bb588bae04fb1615d7461cbba982c0e21
Haaziq Chundrigar <haaziqchundrigar@gmail.com>
2020-04-28 14:02:48 +1000

    Created withHover & Tooltip
    Allows information to be displayed about github username, user company, and user location on hover

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -4,7 +4,7 @@ import { fetchPopularRepos } from '../utils/api'
 import {FaUser, FaStar, FaCodeBranch, FaExclamationTriangle} from 'react-icons/fa'
 import Card from './Card'
 import Loading from './Loading'
-
+import Tooltip from './Tooltip'
 
 function LanguagesNav({selected, onUpdateLanguage}){
 	const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
@@ -46,10 +46,12 @@ function ReposGrid ({ repos }) {
 							>
 								<ul className='card-list'> 
 									<li>
-										<FaUser color = 'rgb(255, 191, 116)' size = {22}/>
-										<a href={'https://github.com/${login}'}>
-											{login}
-										</a>
+										<Tooltip text="Github username">
+											<FaUser color = 'rgb(255, 191, 116)' size = {22}/>
+											<a href={'https://github.com/${login}'}>
+												{login}
+											</a>
+										</Tooltip>
 									</li>
 									<li>
 										<FaStar color = 'rgb(255, 215, 0)' size ={22}/>
