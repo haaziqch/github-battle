b32086f1118ecaf1341b71f0228f5694b6903a24
Haaziq Chundrigar <haaziqchundrigar@gmail.com>
2020-04-30 16:29:35 +1000

    Finalized Application
    Code for production build added
    
    Project is completed

--- app/index.js
+++ app/index.js
@@ -1,44 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import Popular from './components/Popular'
-import Battle from './components/Battle'
-import Results from './components/Results'
 import { ThemeProvider } from './contexts/theme'
 import Nav from './components/Nav'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Loading from './components/Loading'
+
+const Popular = React.lazy(() => import('./components/Popular'))
+const Battle = React.lazy(() => import('./components/Battle'))
+const Results = React.lazy(() => import('./components/Results'))
 
 class App extends React.Component {
-	constructor(props) {
-		super(props)
-
-		this.state = {
-			theme: 'light',
-			toggleTheme: () => {
-				this.setState(({ theme }) => ({
-					theme: theme === 'light' ? 'dark' : 'light'
-				}))
-			}
+	state = {
+		theme: 'light',
+		toggleTheme: () => {
+			this.setState(({ theme }) => ({
+				theme: theme === 'light' ? 'dark' : 'light'
+			}))
 		}
 	}
   render() {
-    return (
-	    <Router>
-				<ThemeProvider value={this.state}>
-			  	<div className={this.state.theme}>
-			      <div className = 'container'>
-			      	<Nav />
-			      	<Switch>
-				        <Route exact path ='/' component={Popular} />
-				        <Route exact path ='/battle' component={Battle} />
-				        <Route path ='/battle/results' component={Results} />
-				        <Route render={() => <h1>404 Page not found</h1>} />
-			      	</Switch>
-			      </div>
-	      	</div>
-		  	</ThemeProvider>
-  		</Router>
-    )
+		return (
+			<Router>
+					<ThemeProvider value={this.state}>
+					<div className={this.state.theme}>
+					  <div className = 'container'>
+						<Nav />
+						<React.Suspense fallback={<Loading/>}>
+							<Switch>
+								<Route exact path ='/' component={Popular} />
+								<Route exact path ='/battle' component={Battle} />
+								<Route path ='/battle/results' component={Results} />
+								<Route render={() => <h1>404 Page not found</h1>} />
+							</Switch>
+						</React.Suspense>
+					  </div>
+				</div>
+				</ThemeProvider>
+			</Router>
+		)
   }
 }
 
